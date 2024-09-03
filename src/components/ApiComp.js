1e77d7710cda58108bbbecde559b88d2c70d9b7d
arkstudiodevelopers <nicksonogm@gmail.com>
2024-09-03 20:38:09 +0300

    Started finalizing on the styles

--- src/components/ApiComp.js
+++ src/components/ApiComp.js
@@ -1,6 +1,8 @@
-import React, { useRef, useState, useEffect } from 'react';
-import axios from 'axios';
-import ExcelJS from 'exceljs';
+import React, { useRef, useState, useEffect } from "react";
+import axios from "axios";
+import ExcelJS from "exceljs";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function ApiComp() {
   const textareaRef = useRef(null);
@@ -12,23 +14,25 @@ function ApiComp() {
       textareaRef.current.focus();
     }
   }, []);
+  AOS.init();
 
   const handleSubmit = async () => {
     const textareaValue = textareaRef.current.value;
-    const ingredients = textareaValue.split('\n');
+    const ingredients = textareaValue.split("\n");
 
     try {
       const response = await axios.post(
-        'https://recipe-food-nutrition15.p.rapidapi.com/nutrition-details',
+        "https://recipe-food-nutrition15.p.rapidapi.com/nutrition-details",
         {
-          ingredients
+          ingredients,
         },
         {
           headers: {
-            'x-rapidapi-key': 'f7171ed0damsh435a225b94967c1p1e2eb6jsn57f2abe28f90',
-            'x-rapidapi-host': 'recipe-food-nutrition15.p.rapidapi.com',
-            'Content-Type': 'application/json'
-          }
+            "x-rapidapi-key":
+              "f7171ed0damsh435a225b94967c1p1e2eb6jsn57f2abe28f90",
+            "x-rapidapi-host": "recipe-food-nutrition15.p.rapidapi.com",
+            "Content-Type": "application/json",
+          },
         }
       );
 
@@ -41,8 +45,8 @@ function ApiComp() {
 
   useEffect(() => {
     if (containerRef.current && responseData) {
-      const groups = containerRef.current.querySelectorAll('.Groups');
-      const largeCard = containerRef.current.querySelector('#Large-card');
+      const groups = containerRef.current.querySelectorAll(".Groups");
+      const largeCard = containerRef.current.querySelector("#Large-card");
       const largeCardHeight = largeCard.offsetHeight;
       groups.forEach((group) => {
         group.style.height = `${largeCardHeight}px`;
@@ -50,53 +54,46 @@ function ApiComp() {
     }
   }, [responseData]);
 
-
   const handleDownloadExcel = () => {
     const workbook = new ExcelJS.Workbook();
-    const worksheet = workbook.addWorksheet('Nutrition Data');
-  
+    const worksheet = workbook.addWorksheet("Nutrition Data");
+
     // Add header row
-    worksheet.addRow([
-      'Category',
-      'Label',
-      'Quantity',
-      'Unit'
-    ]);
-  
+    worksheet.addRow(["Category", "Label", "Quantity", "Unit"]);
+
     // Add data rows
     Object.keys(responseData.totalDaily).forEach((key) => {
       worksheet.addRow([
-        'Total Daily',
+        "Total Daily",
         key,
         responseData.totalDaily[key].quantity,
-        responseData.totalDaily[key].unit
+        responseData.totalDaily[key].unit,
       ]);
     });
-  
+
     Object.keys(responseData.totalNutrients).forEach((key) => {
       worksheet.addRow([
-        'Total Nutrients',
+        "Total Nutrients",
         key,
         responseData.totalNutrients[key].quantity,
-        responseData.totalNutrients[key].unit
+        responseData.totalNutrients[key].unit,
       ]);
     });
-  
+
     // Add footer row
-    worksheet.addRow([
-      'Total Weight:',
-      responseData.totalWeight,
-      'g'
-    ]);
-  
+    worksheet.addRow(["Total Weight:", responseData.totalWeight, "g"]);
+
     // Create a blob and trigger a download
-    workbook.xlsx.writeBuffer()
+    workbook.xlsx
+      .writeBuffer()
       .then((buffer) => {
-        const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+        const blob = new Blob([buffer], {
+          type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        });
         const url = URL.createObjectURL(blob);
-        const a = document.createElement('a');
+        const a = document.createElement("a");
         a.href = url;
-        a.download = 'nutrition_data.xlsx';
+        a.download = "nutrition_data.xlsx";
         a.click();
         URL.revokeObjectURL(url);
       })
@@ -105,12 +102,18 @@ function ApiComp() {
       });
   };
 
-
-
-
   return (
     <div>
-      <p className="Indication-For-textarea">Key in Your ingridients to get a detailed nutritional analysis of your meal</p>
+      <p
+        className="Indication-For-textarea"
+        data-aos="fade-zoom-in"
+        data-aos-easing="ease-in-back"
+        data-aos-delay="100"
+        data-aos-offset="0"
+      >
+        Key in Your ingridients to get a detailed nutritional analysis of your
+        meal
+      </p>
       <textarea
         className="Ingridients-Capture"
         rows="10"
@@ -119,109 +122,157 @@ function ApiComp() {
         required
         wrap="hard"
         ref={textareaRef}
+        data-aos="fade-zoom-in"
+        data-aos-easing="ease-in-back"
+        data-aos-delay="100"
+        data-aos-offset="0"
       />
       <br />
-      <button className="Submit-button" onClick={handleSubmit}>
+      <button
+        className="Submit-button"
+        onClick={handleSubmit}
+        data-aos="fade-zoom-in"
+        data-aos-easing="ease-in-back"
+        data-aos-delay="100"
+        data-aos-offset="0"
+      >
         Submit
       </button>
       {responseData && (
-        <button className="Submit-button" onClick={handleDownloadExcel}>Download Excel</button>
+        <button
+          className="Submit-button"
+          onClick={handleDownloadExcel}
+          data-aos="fade-zoom-in"
+        >
+          Download Excel
+        </button>
       )}
       {responseData ? (
-  <div ref={containerRef}>
-    <div className="Group-container">
-      {/* Group 1: Nutritional Analysis */}
-      <div className="Groups">
-        <h2>Calorie & Labels:</h2>
-        {responseData.calories ? (
-          <p>Calories: {responseData.calories} <br /><br /></p>
-        ) : (
-          <p>No calories data available <br /> <br /></p>
-        )}
-        
-        <ul><br />
-        <h4>Diet Labels</h4>
-        {responseData.dietLabels.slice(0, 9).map((label) => (
-          <li key={label}>{label}</li>
-          ))}
-        </ul><br />
-        
-        <ul>
-          <h4>Health Labels</h4>
-          {responseData.healthLabels.slice(0,9).map((label) => (
-            <li key={label}>{label}</li>
-          ))}
-        </ul><br />
-        <ul>
-        <p>Yield: {responseData.yield}</p>
-        </ul>
-      </div>
-      {/* Group 2: Ingredients */}
-      <div className="Groups">
-        {/* <h2>Ingredients:</h2>
+        <div ref={containerRef} data-aos="fade-up">
+          <div className="Group-container">
+            {/* Group 1: Nutritional Analysis */}
+            <div className="Groups">
+              <h2>Calorie & Labels:</h2>
+              {responseData.calories ? (
+                <p>
+                  Calories: {responseData.calories} <br />
+                  <br />
+                </p>
+              ) : (
+                <p>
+                  No calories data available <br /> <br />
+                </p>
+              )}
+
+              <ul>
+                <br />
+                <h4>Diet Labels</h4>
+                {responseData.dietLabels.slice(0, 9).map((label) => (
+                  <li key={label}>{label}</li>
+                ))}
+              </ul>
+              <br />
+
+              <ul>
+                <h4>Health Labels</h4>
+                {responseData.healthLabels.slice(0, 9).map((label) => (
+                  <li key={label}>{label}</li>
+                ))}
+              </ul>
+              <br />
+              <ul>
+                <p>Yield: {responseData.yield}</p>
+              </ul>
+            </div>
+            {/* Group 2: Ingredients */}
+            <div className="Groups">
+              {/* <h2>Ingredients:</h2>
         <ul>
           {responseData.ingredients.map((ingredient) => (
             <li key={ingredient.text}>{ingredient.text}</li>
           ))}
         </ul> */}
-        <ul>
-          <h2>Total Daily:</h2>
-      {Object.keys(responseData.totalDaily)
-        .slice(0, Math.ceil(Object.keys(responseData.totalDaily).length / 2))
-        .map((key) => (
-          <li key={key}>{key}: {responseData.totalDaily[key].quantity} {responseData.totalDaily[key].unit}</li>
-        ))}
-    </ul>
-      </div>
-      {/* Group 3: Total Daily */}
-      <div className="Groups">
-        <h2>Total Daily:</h2>
-        <ul>
-      {Object.keys(responseData.totalDaily)
-        .slice(Math.ceil(Object.keys(responseData.totalDaily).length / 2))
-        .map((key) => (
-          <li key={key}>{key}: {responseData.totalDaily[key].quantity} {responseData.totalDaily[key].unit}</li>
-        ))}
-    </ul>
-      </div>
-      {/* Group 4: Total Nutrients */}
-      <div className="Groups" id="Large-card">
-        <h2>Total Nutrients:</h2>
-        <div>
-          {Object.keys(responseData.totalNutrients)
-            .slice(0, Math.ceil(Object.keys(responseData.totalNutrients).length / 2))
-            .map((key) => (
-              <p key={key}>{key}: {responseData.totalNutrients[key].quantity}{responseData.totalNutrients[key].unit}</p>
-            ))}
-        </div>
-        
-      </div>
-      {/* Group 5: Total Weight */}
-      <div className="Groups">
-        <h2>Total Nutrients</h2>
-          {Object.keys(responseData.totalNutrients)
-            .slice(Math.ceil(Object.keys(responseData.totalNutrients).length / 2))
-            .map((key) => (
-              <p key={key}>{key}: {responseData.totalNutrients[key].quantity}{responseData.totalNutrients[key].unit}</p>
-            ))}
-            <p>Total Weight:{responseData.totalWeight} g</p>
-        </div>
-      {/* <div className="Groups">
+              <ul>
+                <h2>Total Daily:</h2>
+                {Object.keys(responseData.totalDaily)
+                  .slice(
+                    0,
+                    Math.ceil(Object.keys(responseData.totalDaily).length / 2)
+                  )
+                  .map((key) => (
+                    <li key={key}>
+                      {key}: {responseData.totalDaily[key].quantity}{" "}
+                      {responseData.totalDaily[key].unit}
+                    </li>
+                  ))}
+              </ul>
+            </div>
+            {/* Group 3: Total Daily */}
+            <div className="Groups">
+              <h2>Total Daily:</h2>
+              <ul>
+                {Object.keys(responseData.totalDaily)
+                  .slice(
+                    Math.ceil(Object.keys(responseData.totalDaily).length / 2)
+                  )
+                  .map((key) => (
+                    <li key={key}>
+                      {key}: {responseData.totalDaily[key].quantity}{" "}
+                      {responseData.totalDaily[key].unit}
+                    </li>
+                  ))}
+              </ul>
+            </div>
+            {/* Group 4: Total Nutrients */}
+            <div className="Groups" id="Large-card">
+              <h2>Total Nutrients:</h2>
+              <div>
+                {Object.keys(responseData.totalNutrients)
+                  .slice(
+                    0,
+                    Math.ceil(
+                      Object.keys(responseData.totalNutrients).length / 2
+                    )
+                  )
+                  .map((key) => (
+                    <p key={key}>
+                      {key}: {responseData.totalNutrients[key].quantity}
+                      {responseData.totalNutrients[key].unit}
+                    </p>
+                  ))}
+              </div>
+            </div>
+            {/* Group 5: Total Weight */}
+            <div className="Groups">
+              <h2>Total Nutrients</h2>
+              {Object.keys(responseData.totalNutrients)
+                .slice(
+                  Math.ceil(Object.keys(responseData.totalNutrients).length / 2)
+                )
+                .map((key) => (
+                  <p key={key}>
+                    {key}: {responseData.totalNutrients[key].quantity}
+                    {responseData.totalNutrients[key].unit}
+                  </p>
+                ))}
+              <p>Total Weight:{responseData.totalWeight} g</p>
+            </div>
+            {/* <div className="Groups">
         <h2>Total Weight:</h2>
         <p>{responseData.totalWeight} g</p>
       </div> */}
-      {/* Group 6: URI */}
-      {/* <div className="Groups">
+            {/* Group 6: URI */}
+            {/* <div className="Groups">
         <h2>URI:</h2>
         <p>{responseData.uri}</p>
       </div> */}
-      {/* Group 7: Yield */}
-      {/* <div className="Groups">
+            {/* Group 7: Yield */}
+            {/* <div className="Groups">
         <h2>Yield:</h2>
         <p>{responseData.yield}</p>
       </div> */}
-      {/* Group 8: Cautions */}
-      {/* <div className="Groups">
+            {/* Group 8: Cautions */}
+            {/* <div className="Groups">
         <h2>Cautions:</h2>
         <ul>
           {responseData.cautions.map((caution) => (
@@ -229,11 +280,11 @@ function ApiComp() {
           ))}
         </ul>
       </div> */}
-    </div>
-  </div>
-) : (
-  <p>Loading...</p>
-)}
+          </div>
+        </div>
+      ) : (
+        <p></p>
+      )}
     </div>
   );
 }
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,27 +1,43 @@
-import React from 'react'
+import React from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function Content() {
+  AOS.init();
   return (
     <>
-      <div className='Nav-bar'>
-        <h2 className='Heading'>Nutrition Analysis</h2>
-      </div> {/* Add this closing tag */}
+      <div className="Nav-bar">
+        <h2>Nutrition Analysis</h2>
+      </div>{" "}
+      {/* Add this closing tag */}
+      <div
+        className="Header"
+        data-aos="fade-zoom-in"
+        data-aos-easing="ease-in-back"
+        data-aos-delay="100"
+        data-aos-offset="0"
+      >
+        <p>
+          Good nutrition is the foundation of a healthy lifestyle. What we eat
+          directly impacts our energy levels, overall well-being, and long-term
+          health. Understanding the nutritional content of your meals is crucial
+          for making informed decisions that align with your health goals,
+          whether you're aiming to lose weight, build muscle, or simply maintain
+          a balanced diet.
+        </p>
+        <br />
 
-      <div className='Header'>
-        <p>Good nutrition is the foundation of a healthy lifestyle. What we eat directly impacts our energy levels, 
-          overall well-being, and long-term health. Understanding the nutritional content of your meals is crucial 
-          for making informed decisions that align with your health goals, whether you're aiming to lose weight, build muscle, 
-          or simply maintain a balanced diet.</p>
-          <br />
-
-        <p>This app is designed to simplify this process. It calculates the nutritional breakdown of your meals, 
-          providing detailed insights into calorie count, essential nutrients, vitamins, minerals, and more. With 
-          this information at your fingertips, you can easily track your intake and make adjustments to support your 
-          dietary needs. Empower yourself with knowledge and take control of your nutrition with our app.
+        <p>
+          This app is designed to simplify this process. It calculates the
+          nutritional breakdown of your meals, providing detailed insights into
+          calorie count, essential nutrients, vitamins, minerals, and more. With
+          this information at your fingertips, you can easily track your intake
+          and make adjustments to support your dietary needs. Empower yourself
+          with knowledge and take control of your nutrition with our app.
         </p>
       </div>
     </>
-  )
+  );
 }
 
-export default Content
+export default Content;
